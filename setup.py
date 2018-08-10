7c702e6cb4d1a52361e683750b1799d7beb5650d
tpropson <tpropson@mcs.anl.gov>
2018-08-10 16:42:03 -0500

    added setup.py
    Former-commit-id: af743de5595e951a607e08261ffc60a3e16146fd

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="deephyper",
+    version="0.1dev",
+    packages=["deephyper",],
+    license="NONE",
+    long_description="A package for hyperparameter optimization on "
+                     "deep neural nets."
+)
