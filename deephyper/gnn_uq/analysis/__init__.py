afc5dab5c2a173f8ba45df018835ceadf19166b3
Shengli Jiang <sj0161@della8.princeton.edu>
2024-05-25 15:18:13 -0400

    code update

--- deephyper/gnn_uq/analysis/__init__.py
+++ deephyper/gnn_uq/analysis/__init__.py
@@ -12,14 +12,10 @@ from .result import extract_functional_groups
 from .result import calculate_silhouette_score
 from .calib import calculate_conf_calib
 from .result import get_result_simple
-from .result import get_result_random_simple
 from .result import get_result_mc_dropout
-from .result import get_result_mc_dropout_random
 from .result_pickle import result_to_pickle
 from .result_pickle import random_result_to_pickle
 from .result_pickle import simple_result_to_pickle
-from .result_pickle import simple_random_result_to_pickle
 from .result_pickle import mc_dropout_result_to_pickle
-from .result_pickle import mc_dropout_random_result_to_pickle
 from .result_csv import result_to_csv
 from .comp_time import comp_time
